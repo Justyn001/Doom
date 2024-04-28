bb87f87233b0111226f2aba7aff4f809c28f5d69
Justyn Rojkowski <gw047232@gmail.com>
2024-04-29 00:49:05 +0200

    Initial Game Engine Commit

---
+++ map.py
@@ -0,0 +1,33 @@
+import pygame as pg
+
+_ = False
+mini_map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, 1, 1, 1, 1, _, _, _, 1, 1, 1, _, _, 1],
+    [1, _, _, _, _, _, 1, _, _, _, _, _, 1, _, _, 1],
+    [1, _, _, _, _, _, 1, _, _, _, _, _, 1, _, _, 1],
+    [1, _, _, 1, 1, 1, 1, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, 1, 1, _, _, _, _, _, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ]
+
+
+class Map:
+    def __init__(self, game):
+        self.game = game
+        self.mini_map = mini_map
+        self.world_map = {}
+        self.make_map()
+
+    def make_map(self):
+        for index, row in enumerate(self.mini_map):
+            for position, value in enumerate(row):
+                if value:
+                    self.world_map[(position, index)] = value
+
+    def draw_map(self):
+        self.game.screen.fill('black')
+        for pos in self.world_map.keys():
+            pg.draw.rect(self.game.screen, 'grey', (pos[0] * 100, pos[1] * 100, 100, 100), 2)
